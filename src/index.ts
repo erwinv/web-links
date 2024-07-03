0b409df45d19bd1d11041fd22e33c2bf124333ec
Erwin Villejo <erwinvillejo@gmail.com>
2024-07-03 17:42:34 +0800

    test 302 link with social params

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,21 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/:slug', (c) => {
+  const slug = c.req.param('slug')
+  const search = new URLSearchParams({
+    'socialDescription': 'Blue House Vacuum cleaner BH007M',
+    'socialImageUrl': 'https://media.allonline.7eleven.co.th/Blue_House_Vacuum_cleaner_BH007M/plist/118584_010_AM_01.jpg',
+    'socialTitle': 'Blue House Vacuum cleaner BH007M'
+  })
+  return c.redirect(`/i/${slug}?${search}`, 302)
+})
+
+app.get('/i/:slug', (c) => {
+  const slug = c.req.param('slug')
+  return c.html(`<h1>${slug}</h1>`)
+})
+
 const port = 3001
 console.log(`Server is running on port ${port}`)
 
